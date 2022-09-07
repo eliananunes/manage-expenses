1ca61bf8854a7f1499ab6549c8d3a0a8c208f77c
eliananunes <elianadnunes@gmail.com>
2022-09-07 19:10:25 -0300

    criacao da rota de despesas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
+import Expenses from "./pages/Expenses";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
        <Route path="/" element={<Login/>} />
+       <Route path="/despesas" element={<Expenses />} />
     </Routes>
   );
 }
