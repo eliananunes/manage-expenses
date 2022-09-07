5440c9c78a87ddc8588581f0ab581eb2fd28d587
eliananunes <elianadnunes@gmail.com>
2022-09-07 19:11:55 -0300

    criacao do Header da pagina de Despesas

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header>
+            <h4>Nome</h4>
+            <h4>Login</h4>
+        </header>
+    );
+}
+
+export default Header;
