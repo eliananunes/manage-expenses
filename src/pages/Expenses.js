15d20665f9359f1c2a77d65c1594ace3ee5ba6e5
eliananunes <elianadnunes@gmail.com>
2022-09-07 19:11:14 -0300

    criacao da pagina de despesas

---
+++ src/pages/Expenses.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Expenses = () => {
+
+    return(
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default Expenses;
