3548b4cd99795052ace30209f9261d4120a56d36
eliananunes <elianadnunes@gmail.com>
2022-09-08 17:15:37 -0300

    adiciona formulario e tabela na pagina de despesas

--- src/pages/Expenses.js
+++ src/pages/Expenses.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Form from "../components/Form";
 import Header from "../components/Header";
+import Table from "../components/Table";
 
 const Expenses = () => {
 
     return(
         <div>
             <Header />
+            <Form />
+            <Table />
         </div>
     );
 }
