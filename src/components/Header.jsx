0cd5d7588bb07f54e981b1c5721382f8f31b8148
eliananunes <elianadnunes@gmail.com>
2022-09-09 11:20:10 -0300

    Header: consumindo o estado da aplicacao retornando o nome e email da pessoa usuaria

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../redux/userSlice";
 
 const Header = () => {
+    const { name, email } = useSelector(selectUser);
     return(
         <header>
-            <h4>Nome</h4>
-            <h4>Login</h4>
+            <h4>{name}</h4>
+            <h4>{email}</h4>
         </header>
     );
 }
